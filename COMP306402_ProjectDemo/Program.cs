c6f9c5198dac6415fa40bcb486ee90d9714316df
Sachin Mapa <smapa@my.centennialcollege.ca>
2025-11-23 04:23:06 -0600

    enabled swagger always

--- COMP306402_ProjectDemo/Program.cs
+++ COMP306402_ProjectDemo/Program.cs
@@ -29,12 +29,9 @@ builder.Services.AddScoped<IEnrollmentRepository, EnrollmentRepository>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+// Enable Swagger always
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
